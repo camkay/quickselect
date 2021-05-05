6f7f7e13db7bf75751b203d8cd71644b73546be9
Cameron Kay <ckay@uoregon.edu>
2021-05-04 19:02:05 -0700

    Fixed the imports. Incremented to version 0.1.0.

--- R/match.R
+++ R/match.R
@@ -3,12 +3,14 @@
 #' `match` extracts columns in a data frame that match a specified regex string.
 #' @param .data a data frame or tibble.
 #' @param pattern a character string that is used to find the columns of interest. It can be a regular expression.
+#' @importFrom dplyr select
+#' @importFrom dplyr matches
 #' @export
 #' 
 
 match <- function(.data, pattern) {
   # match columns
-  out <- dplyr::select(.data, matches(pattern))
+  out <- select(.data, matches(pattern))
   
   # return out
   out
